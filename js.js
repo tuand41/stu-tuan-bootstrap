a290cebb0be302d9d5f40b99165f406506174c6e
tuand4 <vantuantk4@gmail.com>
2019-12-15 23:21:07 +0700

    1

---
+++ js.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $('.slide-sukien').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"> abc </button>',
+    nextArrow: '<button type="button" class="slick-next"> abc </button>',
+    centerMode: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
+});
