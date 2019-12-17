c830b4c273f0082e93f3f54f93978532b2fc8deb
tuand4 <vantuantk4@gmail.com>
2019-12-17 15:45:24 +0700

    tab

--- js.js
+++ js.js
@@ -14,4 +14,23 @@ $(document).ready(function () {
     $('.mb-left').toggle("slow");
   });
 
+
+  $('#dv-ct-2').hide();
+  $('#tab-dv-1').click(function(){
+    $(this).addClass('active');
+    $('#tab-dv-2').removeClass('active');
+    $('#dv-ct-1').show();
+    $('#dv-ct-2').hide();
+  });
+
+  $('#tab-dv-2').click(function(){
+    $(this).addClass('active');
+    $('#tab-dv-1').removeClass('active');
+    $('#dv-ct-1').hide();
+    $('#dv-ct-2').show();
+  });
+
 });
+
+
+
